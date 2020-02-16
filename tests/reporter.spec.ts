8651e47fa154c8c16f07ded780b1f9498ed4ea57
Monika Protivová <monika.protivova@cleverfarm.cz>
2020-02-17 00:03:50 +0100

    updated tests

--- tests/reporter.spec.ts
+++ tests/reporter.spec.ts
@@ -1,10 +1,9 @@
 import {
     BufferedWriter,
     CucumberConsoleReporter,
-    DefaultFormatter,
     FailedStep,
     Formatter,
-    Result,
+    Result, Stage,
 } from "../src";
 
 import * as sinon from 'sinon';
@@ -42,20 +41,21 @@ describe('CucumberConsoleReporter', () => {
     };
 
     const reporter = new CucumberConsoleReporter(reporterOptions, BufferedWriter, SimpleTestFormatter);
-    const writerSpy = sinon.spy(reporter.logger, 'log');
+    const logSpy = sinon.spy(reporter.logger, 'log');
+    const finalizeSpy = sinon.spy(reporter.logger, 'finalize');
 
     describe('onSuiteStart', () => {
 
         it('should log a scenario', () => {
             const suite = { type: 'scenario', title: 'My scenario or whatever' };
             reporter.onSuiteStart(suite);
-            writerSpy.should.have.been.calledWith(suite.title);
+            logSpy.should.have.been.calledWith(suite.title);
         });
 
         it('should log a feature', () => {
             const suite = { type: 'feature', title: 'My feature' };
             reporter.onSuiteStart(suite);
-            writerSpy.should.have.been.calledWith(suite.title);
+            logSpy.should.have.been.calledWith(suite.title);
         });
 
         it('should not log a urecognized type', () => {
@@ -70,42 +70,57 @@ describe('CucumberConsoleReporter', () => {
         it('should log a passing step', () => {
             const test = { title: 'My passing step' };
             reporter.onTestPass(test);
-            writerSpy.should.have.been.calledWith(`${test.title} ${Result.PASSED}`);
+            logSpy.should.have.been.calledWith(`${test.title} ${Result.PASSED}`);
+            finalizeSpy.should.have.been.calledWith(Result.PASSED);
         });
 
     });
 
-    // describe('onTestFail', () => {
-    //
-    //     it('should log a failing step', () => {
-    //         const test = { title: 'My failing step' };
-    //         reporter.onTestFail(test);
-    //         writerSpy.should.have.been.calledWith(`${test.title} ${Result.FAILED}`);
-    //     });
-    //
-    // });
+    describe('onTestFail', () => {
+
+        it('should log a failing step', () => {
+            const test = { title: 'My failing step', errors: [new Error('xxx')] };
+            reporter.onTestFail(test);
+            logSpy.should.have.been.calledWith(`${test.title} ${Result.FAILED}`);
+            finalizeSpy.should.have.been.calledWith(Stage.STEP);
+        });
+
+    });
 
     describe('onTestSkip', () => {
 
         it('should log a skipped step', () => {
             const test = { title: 'My skipped step' };
             reporter.onTestSkip(test);
-            writerSpy.should.have.been.calledWith(`${test.title} ${Result.SKIPPED}`);
+            logSpy.should.have.been.calledWith(`${test.title} ${Result.SKIPPED}`);
+            finalizeSpy.should.have.been.calledWith(Stage.STEP);
         });
 
     });
 
-    // describe('onSuiteEnd', () => {
-    //
-    //     it('should log a run summary', () => {
-    //         const suite = { title: 'My skipped step' };
-    //         reporter.onSuiteEnd(suite);
-    //         writerSpy.should.have.been.calledWith(`XXX`);
-    //     });
-    //
-    // });
+    describe('onSuiteEnd', () => {
+
+        it('should log a run summary', () => {
+            const suite = { tests: [ {state: 'passed'}, {state: 'failed'}, {state: 'skipped'} ] };
+            reporter.onSuiteEnd(suite);
+            logSpy.should.have.been.calledWith('\n');
+            finalizeSpy.should.have.been.calledWith(Stage.STEP);
+        });
+
+    });
 
     describe('onRunnerEnd', () => {
+
+        it('should finalize test', () => {
+            const suite = { tests: [ {state: 'passed'}, {state: 'failed'}, {state: 'skipped'} ] };
+            reporter.onRunnerEnd();
+            logSpy.should.have.been.calledWith('My scenario or whatever');
+            logSpy.should.have.been.calledWith('My feature');
+            logSpy.should.have.been.calledWith('My passing step 0');
+            logSpy.should.have.been.calledWith('My failing step 1');
+            logSpy.should.have.been.calledWith('My skipped step 2');
+            finalizeSpy.should.have.been.calledWith(Stage.STEP);
+        });
 
     });
 
